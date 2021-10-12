b426234d2db6a3c60c8f7721c6eeeac17e08fb49
Vlasov Pavel <pacha_vlasov_95@mail.ru>
2021-10-12 06:10:40 +0300

    Изменил структуру бд

--- app/src/main/java/com/example/sqllite/DBHelper.java
+++ app/src/main/java/com/example/sqllite/DBHelper.java
@@ -3,16 +3,32 @@ package com.example.sqllite;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class DBHelper  extends SQLiteOpenHelper{
-
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "contactDb";
-    public static final String TABLE_CONTACTS = "contacts";
+    public static final int DATABASE_VERSION = 2;
+    public static final String DATABASE_NAME = "menuDb";
+    public static final String TABLE_MENU = "menu";
 
     public static final String KEY_ID = "_id";
-    public static final String KEY_NAME = "name";
-    public static final String KEY_MAIL = "mail";
+    public static final String KEY_DISH = "dish";
+    public static final String KEY_PRICE = "price";
+    public static final String KEY_COMPOSITION = "composition";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,14 +36,14 @@ public class DBHelper  extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
-                + " integer primary key," + KEY_NAME + " text," + KEY_MAIL + " text" + ")");
+        db.execSQL("create table " + TABLE_MENU + "(" + KEY_ID
+                + " integer primary key," + KEY_DISH + " text," + KEY_COMPOSITION  + " text," + KEY_PRICE + " text"+")");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists " + TABLE_CONTACTS);
+        db.execSQL("drop table if exists " + TABLE_MENU);
 
         onCreate(db);
 
--- app/src/main/java/com/example/sqllite/MainActivity.java
+++ app/src/main/java/com/example/sqllite/MainActivity.java
@@ -5,17 +5,21 @@ import android.os.Bundle;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button btnAdd, btnRead, btnClear;
-    EditText etName, etEmail;
-    TextView textView;
+    Button btnAdd, btnClear;
+    EditText etDish, etPrice, etComposition;
+    SQLiteDatabase database;
+    ContentValues contentValues;
     DBHelper dbHelper;
 
     @Override
@@ -26,64 +30,125 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(this);
 
-        btnRead = (Button) findViewById(R.id.btnRead);
-        btnRead.setOnClickListener(this);
-
         btnClear = (Button) findViewById(R.id.btnClear);
         btnClear.setOnClickListener(this);
 
-        etName = (EditText) findViewById(R.id.etName);
-        etEmail = (EditText) findViewById(R.id.etEmail);
-
-        textView = (TextView) findViewById(R.id.TextBox);
-
-        dbHelper = new com.example.sqllite.DBHelper(this);
+        etDish = (EditText) findViewById(R.id.etDish);
+        etPrice = (EditText) findViewById(R.id.etPrice);
+        etComposition = (EditText) findViewById(R.id.etComposition);
+        dbHelper = new DBHelper(this);
+        database = dbHelper.getWritableDatabase();
+        UpdateTable();
     }
+public void UpdateTable ()
+{
+    Cursor cursor = database.query(DBHelper.TABLE_MENU, null, null, null, null, null, null);
 
+    if (cursor.moveToFirst()) {
+        int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+        int dishIndex = cursor.getColumnIndex(DBHelper.KEY_DISH);
+        int priceIndex = cursor.getColumnIndex(DBHelper.KEY_PRICE);
+        int compositionIndex = cursor.getColumnIndex(DBHelper.KEY_COMPOSITION);
+        TableLayout dpOutput = findViewById(R.id.dpOutput);
+        dpOutput.removeAllViews();
+        do {
+            TableRow dpOutputRow = new TableRow(this);
+            dpOutputRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            LinearLayout.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+            TextView outputDish = new TextView(this);
+            params.weight = 3.0f;
+            outputDish.setLayoutParams(params);
+            outputDish.setText(cursor.getString(dishIndex));
+            dpOutputRow.addView(outputDish);
+
+            TextView outputPrice = new TextView(this);
+            params.weight = 3.0f;
+            outputPrice.setLayoutParams(params);
+            outputPrice.setText(cursor.getString(priceIndex));
+            dpOutputRow.addView(outputPrice);
+
+            TextView outputComposition = new TextView(this);
+            params.weight = 3.0f;
+            outputComposition.setLayoutParams(params);
+            outputComposition.setText(cursor.getString(compositionIndex));
+            dpOutputRow.addView(outputComposition);
+
+            Button deleteBtn = new Button(this);
+            deleteBtn.setOnClickListener(this);
+            params.weight =1.0f;
+            deleteBtn.setLayoutParams(params);
+            deleteBtn.setText("Удалить");
+            deleteBtn.setId(cursor.getInt(idIndex));
+            dpOutputRow.addView(deleteBtn);
+
+            dpOutput.addView(dpOutputRow);
+
+        } while (cursor.moveToNext());
+        cursor.close();
+    }
+}
     @Override
     public void onClick(View v) {
 
-        String name = etName.getText().toString();
-        String email = etEmail.getText().toString();
 
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();
-
-
+        dbHelper = new DBHelper(this);
         switch (v.getId()) {
 
             case R.id.btnAdd:
-                contentValues.put(com.example.sqllite.DBHelper.KEY_NAME, name);
-                contentValues.put(com.example.sqllite.DBHelper.KEY_MAIL, email);
+                String dish = etDish.getText().toString();
+                String price = etPrice.getText().toString();
+                String composition = etComposition.getText().toString();
+                contentValues = new ContentValues();
+                contentValues.put(DBHelper.KEY_COMPOSITION, composition);
+                contentValues.put(DBHelper.KEY_PRICE, price);
+                contentValues.put(DBHelper.KEY_DISH, dish);
+                etDish.setText("");
+                etPrice.setText("");
+                etComposition.setText("");
 
-                database.insert(com.example.sqllite.DBHelper.TABLE_CONTACTS, null, contentValues);
-                break;
+                database.insert(DBHelper.TABLE_MENU, null, contentValues);
+                UpdateTable();
 
-            case R.id.btnRead:
-                Cursor cursor = database.query(com.example.sqllite.DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
-
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(com.example.sqllite.DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(com.example.sqllite.DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(com.example.sqllite.DBHelper.KEY_MAIL);
-                    String s="";
-                    do {
-                        Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex));
-                        s +="\nname = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex);
-                    } while (cursor.moveToNext());
-                    textView.setText(s);
-                } else
-                    Log.d("mLog","0 rows");
-
-                cursor.close();
                 break;
 
             case R.id.btnClear:
-                database.delete(com.example.sqllite.DBHelper.TABLE_CONTACTS, null, null);
+                TableLayout dpOutput = findViewById(R.id.dpOutput);
+                dpOutput.removeAllViews();
+                database.delete(DBHelper.TABLE_MENU, null, null);
+                UpdateTable();
+                break;
+            default:
+                View outputDBRow = (View) v.getParent();
+                ViewGroup outputDB = (ViewGroup) outputDBRow.getParent();
+                outputDB.removeView(outputDBRow);
+                outputDB.invalidate();
+
+                database.delete(DBHelper.TABLE_MENU,dbHelper.KEY_ID+" = ?",new String[]{ String.valueOf(v.getId())});
+                contentValues = new ContentValues();
+                Cursor cursorUpdater = database.query(DBHelper.TABLE_MENU, null, null, null, null, null, null);
+                if (cursorUpdater.moveToFirst()) {
+                    int idIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_ID);
+                    int dishIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_DISH);
+                    int priceIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_PRICE);
+                    int compositionIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_COMPOSITION);
+                    int realID = 1;
+                    do {
+                        if (cursorUpdater.getInt(idIndex) > realID) {
+                            contentValues.put(DBHelper.KEY_ID, realID);
+                            contentValues.put(DBHelper.KEY_DISH, cursorUpdater.getString(dishIndex));
+                            contentValues.put(DBHelper.KEY_PRICE, cursorUpdater.getString(priceIndex));
+                            contentValues.put(DBHelper.KEY_COMPOSITION, cursorUpdater.getString(compositionIndex));
+                            database.replace(DBHelper.TABLE_MENU, null, contentValues);
+                        }
+                        realID++;
+                    } while (cursorUpdater.moveToNext());
+                    if (cursorUpdater.moveToLast()) {
+                        database.delete(dbHelper.TABLE_MENU, DBHelper.KEY_ID + " = ?", new String[]{cursorUpdater.getString(idIndex)});
+                    }
+                    UpdateTable();
+                }
                 break;
         }
         dbHelper.close();
