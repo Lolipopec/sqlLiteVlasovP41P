ad4fb6cdf3b364c248de5d3fbd7614930c9ea0d2
Lolipop <pacha_vlasov_95@mail.ru>
2021-10-24 21:03:09 +0300

    Ну типа да

--- app/src/main/java/com/example/sqllite/DBHelper.java
+++ app/src/main/java/com/example/sqllite/DBHelper.java
@@ -21,14 +21,13 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 public class DBHelper  extends SQLiteOpenHelper{
-    public static final int DATABASE_VERSION = 2;
-    public static final String DATABASE_NAME = "menuDb";
-    public static final String TABLE_MENU = "menu";
+    public static final int DATABASE_VERSION = 3;
+    public static final String DATABASE_NAME = "catalogDb";
+    public static final String TABLE_CATALOG = "catalog";
 
     public static final String KEY_ID = "_id";
-    public static final String KEY_DISH = "dish";
+    public static final String KEY_PRODUCT = "product";
     public static final String KEY_PRICE = "price";
-    public static final String KEY_COMPOSITION = "composition";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -36,14 +35,14 @@ public class DBHelper  extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_MENU + "(" + KEY_ID
-                + " integer primary key," + KEY_DISH + " text," + KEY_COMPOSITION  + " text," + KEY_PRICE + " text"+")");
+        db.execSQL("create table " + TABLE_CATALOG + "(" + KEY_ID
+                + " integer primary key," + KEY_PRODUCT + " text," + KEY_PRICE + " text"+")");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists " + TABLE_MENU);
+        db.execSQL("drop table if exists " + TABLE_CATALOG);
 
         onCreate(db);
 
--- app/src/main/java/com/example/sqllite/MainActivity.java
+++ app/src/main/java/com/example/sqllite/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.sqllite;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -13,14 +15,16 @@ import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnAdd, btnClear;
-    EditText etDish, etPrice, etComposition;
+    EditText etProduct, etPrice;
     SQLiteDatabase database;
     ContentValues contentValues;
     DBHelper dbHelper;
+    TextView tvBasket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,126 +34,143 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(this);
 
+
         btnClear = (Button) findViewById(R.id.btnClear);
         btnClear.setOnClickListener(this);
 
-        etDish = (EditText) findViewById(R.id.etDish);
+        etProduct = (EditText) findViewById(R.id.etProduct);
         etPrice = (EditText) findViewById(R.id.etPrice);
-        etComposition = (EditText) findViewById(R.id.etComposition);
+        tvBasket = (TextView) findViewById(R.id.tvBasket);
+        tvBasket.setOnClickListener(this);
         dbHelper = new DBHelper(this);
         database = dbHelper.getWritableDatabase();
         UpdateTable();
     }
-public void UpdateTable ()
-{
-    Cursor cursor = database.query(DBHelper.TABLE_MENU, null, null, null, null, null, null);
 
-    if (cursor.moveToFirst()) {
-        int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-        int dishIndex = cursor.getColumnIndex(DBHelper.KEY_DISH);
-        int priceIndex = cursor.getColumnIndex(DBHelper.KEY_PRICE);
-        int compositionIndex = cursor.getColumnIndex(DBHelper.KEY_COMPOSITION);
-        TableLayout dpOutput = findViewById(R.id.dpOutput);
-        dpOutput.removeAllViews();
-        do {
-            TableRow dpOutputRow = new TableRow(this);
-            dpOutputRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+    public void UpdateTable() {
+        Cursor cursor = database.query(DBHelper.TABLE_CATALOG, null, null, null, null, null, null);
 
-            LinearLayout.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+        if (cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+            int dishIndex = cursor.getColumnIndex(DBHelper.KEY_PRODUCT);
+            int priceIndex = cursor.getColumnIndex(DBHelper.KEY_PRICE);
+            TableLayout dpOutput = findViewById(R.id.dpOutput);
+            dpOutput.removeAllViews();
+            do {
+                TableRow dpOutputRow = new TableRow(this);
+                dpOutputRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                LinearLayout.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+                TextView outputDish = new TextView(this);
+                params.weight = 3.0f;
+                outputDish.setLayoutParams(params);
+                outputDish.setText(cursor.getString(dishIndex));
+                dpOutputRow.addView(outputDish);
 
-            TextView outputDish = new TextView(this);
-            params.weight = 3.0f;
-            outputDish.setLayoutParams(params);
-            outputDish.setText(cursor.getString(dishIndex));
-            dpOutputRow.addView(outputDish);
+                TextView outputPrice = new TextView(this);
+                params.weight = 3.0f;
+                outputPrice.setLayoutParams(params);
+                outputPrice.setText(cursor.getString(priceIndex));
+                dpOutputRow.addView(outputPrice);
 
-            TextView outputPrice = new TextView(this);
-            params.weight = 3.0f;
-            outputPrice.setLayoutParams(params);
-            outputPrice.setText(cursor.getString(priceIndex));
-            dpOutputRow.addView(outputPrice);
+                Button deleteBtn = new Button(this);
+                deleteBtn.setOnClickListener(this);
+                params.weight = 1.0f;
+                deleteBtn.setLayoutParams(params);
+                deleteBtn.setText("Удалить");
+                deleteBtn.setId(cursor.getInt(idIndex));
+                dpOutputRow.addView(deleteBtn);
 
-            TextView outputComposition = new TextView(this);
-            params.weight = 3.0f;
-            outputComposition.setLayoutParams(params);
-            outputComposition.setText(cursor.getString(compositionIndex));
-            dpOutputRow.addView(outputComposition);
+                Button buyBtn = new Button(this);
+                buyBtn.setOnClickListener(this);
+                params.weight = 1.0f;
+                buyBtn.setLayoutParams(params);
+                buyBtn.setText("Купить");
+                buyBtn.setId(cursor.getInt(idIndex));
+                dpOutputRow.addView(buyBtn);
 
-            Button deleteBtn = new Button(this);
-            deleteBtn.setOnClickListener(this);
-            params.weight =1.0f;
-            deleteBtn.setLayoutParams(params);
-            deleteBtn.setText("Удалить");
-            deleteBtn.setId(cursor.getInt(idIndex));
-            dpOutputRow.addView(deleteBtn);
+                dpOutput.addView(dpOutputRow);
 
-            dpOutput.addView(dpOutputRow);
-
-        } while (cursor.moveToNext());
-        cursor.close();
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
     }
-}
+
     @Override
     public void onClick(View v) {
 
 
         dbHelper = new DBHelper(this);
         switch (v.getId()) {
-
-            case R.id.btnAdd:
-                String dish = etDish.getText().toString();
-                String price = etPrice.getText().toString();
-                String composition = etComposition.getText().toString();
-                contentValues = new ContentValues();
-                contentValues.put(DBHelper.KEY_COMPOSITION, composition);
-                contentValues.put(DBHelper.KEY_PRICE, price);
-                contentValues.put(DBHelper.KEY_DISH, dish);
-                etDish.setText("");
-                etPrice.setText("");
-                etComposition.setText("");
-
-                database.insert(DBHelper.TABLE_MENU, null, contentValues);
-                UpdateTable();
-
+            case R.id.tvBasket:
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Сумма "+tvBasket.getText(), Toast.LENGTH_SHORT);
+                toast.show();
+                tvBasket.setText("0");
                 break;
-
+            case R.id.btnAdd:
+                String dish = etProduct.getText().toString();
+                String price = etPrice.getText().toString();
+                contentValues = new ContentValues();
+                contentValues.put(DBHelper.KEY_PRICE, price);
+                contentValues.put(DBHelper.KEY_PRODUCT, dish);
+                etProduct.setText("");
+                etPrice.setText("");
+                database.insert(DBHelper.TABLE_CATALOG, null, contentValues);
+                UpdateTable();
+                break;
             case R.id.btnClear:
                 TableLayout dpOutput = findViewById(R.id.dpOutput);
                 dpOutput.removeAllViews();
-                database.delete(DBHelper.TABLE_MENU, null, null);
+                database.delete(DBHelper.TABLE_CATALOG, null, null);
                 UpdateTable();
                 break;
             default:
-                View outputDBRow = (View) v.getParent();
-                ViewGroup outputDB = (ViewGroup) outputDBRow.getParent();
-                outputDB.removeView(outputDBRow);
-                outputDB.invalidate();
-
-                database.delete(DBHelper.TABLE_MENU,dbHelper.KEY_ID+" = ?",new String[]{ String.valueOf(v.getId())});
-                contentValues = new ContentValues();
-                Cursor cursorUpdater = database.query(DBHelper.TABLE_MENU, null, null, null, null, null, null);
-                if (cursorUpdater.moveToFirst()) {
-                    int idIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_ID);
-                    int dishIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_DISH);
-                    int priceIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_PRICE);
-                    int compositionIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_COMPOSITION);
-                    int realID = 1;
-                    do {
-                        if (cursorUpdater.getInt(idIndex) > realID) {
-                            contentValues.put(DBHelper.KEY_ID, realID);
-                            contentValues.put(DBHelper.KEY_DISH, cursorUpdater.getString(dishIndex));
-                            contentValues.put(DBHelper.KEY_PRICE, cursorUpdater.getString(priceIndex));
-                            contentValues.put(DBHelper.KEY_COMPOSITION, cursorUpdater.getString(compositionIndex));
-                            database.replace(DBHelper.TABLE_MENU, null, contentValues);
+                Button btn = (Button) v;
+                switch (btn.getText().toString()) {
+                    case "Купить":
+                        String selection = "_id = ?";
+                        Cursor c = database.query(DBHelper.TABLE_CATALOG, null, selection, new String[]{String.valueOf(v.getId())}, null, null, null);
+                        float sum = Float.valueOf(tvBasket.getText().toString());
+                        float s = 0;
+                        if (c.moveToFirst()) {
+                                int Price = c.getColumnIndex(DBHelper.KEY_PRICE);
+                                do {
+                                    s = c.getFloat(Price);
+                                } while (c.moveToNext());
                         }
-                        realID++;
-                    } while (cursorUpdater.moveToNext());
-                    if  (cursorUpdater.moveToLast() && v.getId()!=realID){
-                        database.delete(dbHelper.TABLE_MENU, DBHelper.KEY_ID + " = ?", new String[]{cursorUpdater.getString(idIndex)});
-                    }
-                    UpdateTable();
+                        c.close();
+                        sum= sum + s;
+                        tvBasket.setText(String.valueOf(sum));
+                        break;
+                    case "Удалить":
+                        View outputDBRow = (View) v.getParent();
+                        ViewGroup outputDB = (ViewGroup) outputDBRow.getParent();
+                        outputDB.removeView(outputDBRow);
+                        outputDB.invalidate();
+                        database.delete(DBHelper.TABLE_CATALOG, dbHelper.KEY_ID + " = ?", new String[]{String.valueOf(v.getId())});
+                        contentValues = new ContentValues();
+                        Cursor cursorUpdater = database.query(DBHelper.TABLE_CATALOG, null, null, null, null, null, null);
+                        if (cursorUpdater.moveToFirst()) {
+                            int idIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_ID);
+                            int dishIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_PRODUCT);
+                            int priceIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_PRICE);
+                            int realID = 1;
+                            do {
+                                if (cursorUpdater.getInt(idIndex) > realID) {
+                                    contentValues.put(DBHelper.KEY_ID, realID);
+                                    contentValues.put(DBHelper.KEY_PRODUCT, cursorUpdater.getString(dishIndex));
+                                    contentValues.put(DBHelper.KEY_PRICE, cursorUpdater.getString(priceIndex));
+                                    database.replace(DBHelper.TABLE_CATALOG, null, contentValues);
+                                }
+                                realID++;
+                            } while (cursorUpdater.moveToNext());
+                            if (cursorUpdater.moveToLast() && v.getId() != realID) {
+                                database.delete(dbHelper.TABLE_CATALOG, DBHelper.KEY_ID + " = ?", new String[]{cursorUpdater.getString(idIndex)});
+                            }
+                            UpdateTable();
+                        }
+                        break;
                 }
-                break;
         }
         dbHelper.close();
     }
